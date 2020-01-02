a88d7d4f92aa2ae5412920f0c391596a5873e262
mylove90 <engm.saad1990@gmail.com>
2020-01-02 03:18:23 +0100

    onclite: MSM Xtended Bringup

--- xtended_onclite.mk
+++ xtended_onclite.mk
@@ -12,14 +12,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 # Inherit from onclite device
 $(call inherit-product, device/xiaomi/onclite/device.mk)
 
-# Inherit some AEX stuff.
-$(call inherit-product, vendor/aosp/common.mk)
+# Inherit some Xtended stuff.
+$(call inherit-product, vendor/xtended/config/common_full_phone.mk)
 
 # Bootanimation res
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_onclite
+PRODUCT_NAME := xtended_onclite
 PRODUCT_DEVICE := onclite
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 7
