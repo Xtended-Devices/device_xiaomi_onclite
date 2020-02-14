c09be11bd5abbba76d3de1129548ec49a5551f4f
Jorg3Lucas <jorgelucas.sd@gmail.com>
2020-02-14 11:18:43 +0200

    onclite: Update coral fingerprint to feb patch

--- xtended_onclite.mk
+++ xtended_onclite.mk
@@ -29,7 +29,7 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 7
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "google/crosshatch/crosshatch:9/PQ3A.190801.002/5670241:user/release-keys"
+BUILD_FINGERPRINT := "google/coral/coral:10/QQ1B.200205.002/6084387:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="onc-user 9 PKQ1.181021.001 V10.3.4.0.PFLMIXM release-keys" \
