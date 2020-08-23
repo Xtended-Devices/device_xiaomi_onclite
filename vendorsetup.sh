b5a20ed211373d3119b98123934336196154ec4d
mylove90 <engm.saad1990@gmail.com>
2020-08-23 17:34:30 +0200

    vendorsetup.sh: wget anx camera apk

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,4 +4,6 @@ git reset --hard xtended/xq
 git fetch https://github.com/Project-Xtended/frameworks_av xq-anp
 git cherry-pick d4b819c79fedc19bda1c565969f2cc39981a5503
 git cherry-pick cf5b100a734071d1698564911254783f1eae9ea1
-cd ../..
+cd MY_ANDROID_DIR/vendor/MiuiCamera/system/priv-app/MiuiCamera/ && rm MiuiCamera.apk
+wget https://github.com/Xtended-Devices/vendor_MiuiCamera/raw/xq-onclite/system/priv-app/MiuiCamera/MiuiCamera.apk
+cd MY_ANDROID_DIR
