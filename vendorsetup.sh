ff576315a5782ff867de405f14e94778b412b3b9
xawlw <46276990+xawlw@users.noreply.github.com>
2020-08-24 23:12:03 +0300

    Fix build error smh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,9 +1,11 @@
-MY_ANDROID_DIR=$PWD
-cd $MY_ANDROID_DIR/frameworks/av
+rm -rf hardware/qcom-caf/sm8150/display
+rm -rf hardware/qcom-caf/msm8996/display
+git clone https://github.com/omnirom/android_hardware_qcom_display-caf-msm8996 hardware/qcom-caf/msm8996/display
+cd frameworks/av
 git reset --hard xtended/xq
 git fetch https://github.com/Project-Xtended/frameworks_av xq-anp
 git cherry-pick d4b819c79fedc19bda1c565969f2cc39981a5503
 git cherry-pick cf5b100a734071d1698564911254783f1eae9ea1
-cd MY_ANDROID_DIR/vendor/MiuiCamera/system/priv-app/MiuiCamera/ && rm MiuiCamera.apk
+cd ../../vendor/MiuiCamera/system/priv-app/MiuiCamera/ && rm MiuiCamera.apk
 wget https://github.com/Xtended-Devices/vendor_MiuiCamera/raw/xq-onclite/system/priv-app/MiuiCamera/MiuiCamera.apk
-cd MY_ANDROID_DIR
+cd ../../../../..
