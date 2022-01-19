a024a04a675bf916d1b1377fa1bd8a1e02a2d81e
afterallafk <shivamatiet2001@gmail.com>
2022-01-19 11:50:49 +0530

    onclite: Update Build FP/Priv Description

--- xtended_onclite.mk
+++ xtended_onclite.mk
@@ -25,10 +25,10 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 7
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "xiaomi/onc/onc:9/PKQ1.181021.001/V11.0.3.0.PFLMIXM:user/release-keys"
+BUILD_FINGERPRINT := "google/raven/raven:12/SQ1D.220105.007/8030436:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="onc-user 9 PKQ1.181021.001 V11.0.3.0.PFLMIXM release-keys"
+    PRIVATE_BUILD_DESC="raven-user 12 SQ1D.220105.007 8030436 release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
